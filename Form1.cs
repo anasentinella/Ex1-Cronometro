89516ec6df1b4237e33521c2ad6a7524fef0842f
anasentinella <anajuliasentinella@gmail.com>
2022-08-17 15:18:24 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1_Cronometro
+{
+    public partial class Form1 : Form
+    {
+        DateTime inicio, fim;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+         private void Form1_Paint(object sender,PaintEventArgs e)
+        {
+            GraphicsPath redondo = new GraphicsPath();
+            redondo.AddEllipse(0, 0, 50, 50);
+            button1.Region = new Region(redondo);
+            button2.Region = new Region(redondo);
+            button3.Region = new Region(redondo);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inicio = DateTime.Now;
+            timer1.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "00.00.00";
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            fim = DateTime.Now;
+            TimeSpan dif = fim.Subtract(inicio);
+            label1.Text = dif.ToString("hh\\:mm\\:ss");
+        }
+    }
+}
